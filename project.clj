370c2c1b2ddc00683a042796a30b8cca552cb71a
Peter Monks <pmonks@gmail.com>
2014-02-21 10:50:37 -0800

    Fixed license in README

--- project.clj
+++ project.clj
@@ -12,8 +12,8 @@
 (defproject org.clojars.pmonks/multigrep "0.2.0-SNAPSHOT"
   :description        "A little library that provides various kinds of regex-based file grepping."
   :url                "https://github.com/pmonks/multigrep"
-  :license {:name     "Creative Commons Attribution-ShareAlike 3.0 Unported License."
-            :url      "http://creativecommons.org/licenses/by-sa/3.0/"}
+  :license            {:name "Eclipse Public License"
+                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version   "2.0.0"
   :dependencies       [[org.clojure/clojure "1.5.1"]]
   :profiles           {:dev     {:dependencies [[midje      "1.6.2"]]
