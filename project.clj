bf0a2bbca2dc42586e81229f3f748cf4af1248d6
Peter Monks <pmonks@gmail.com>
2014-04-08 12:16:00 -0700

    Reintroduced clojure dependency

--- project.clj
+++ project.clj
@@ -15,6 +15,9 @@
   :license            {:name "Eclipse Public License"
                        :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version   "2.0.0"
+  :dependencies       [
+                        [org.clojure/clojure "1.6.0"]
+                      ]
   :profiles           {:dev     {:dependencies [[midje "1.6.3"]]}
                        :uberjar {:aot :all}}
   :uberjar-merge-with {#"META-INF/services/.*" [slurp str spit]}   ; Awaiting Leiningen 2.3.5 - see https://github.com/technomancy/leiningen/issues/1455
