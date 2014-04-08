6c0c2ef32788df58d62fa6fd89e12855c1eb3b62
Peter Monks <pmonks@gmail.com>
2014-04-08 11:36:37 -0700

    Cleaned up project.clj

--- project.clj
+++ project.clj
@@ -15,9 +15,7 @@
   :license            {:name "Eclipse Public License"
                        :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version   "2.0.0"
-  :dependencies       [[org.clojure/clojure "1.5.1"]]
-  :profiles           {:dev     {:dependencies [[midje      "1.6.3"]]
-                                 :plugins      [[lein-midje "3.0.1"]]}
+  :profiles           {:dev     {:dependencies [[midje "1.6.3"]]}
                        :uberjar {:aot :all}}
-  :uberjar-merge-with {#"META-INF/services/.*" [slurp str spit]}   ; See https://github.com/technomancy/leiningen/issues/1455
+  :uberjar-merge-with {#"META-INF/services/.*" [slurp str spit]}   ; Awaiting Leiningen 2.3.5 - see https://github.com/technomancy/leiningen/issues/1455
   )
