a9f05048c63ce565c7d654dd6c75d6b205cf19f0
Peter Monks <pmonks@gmail.com>
2017-08-30 23:18:07 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                          [org.clojure/clojure "1.8.0"]
                        ]
   :profiles            {:dev     {:dependencies [[midje      "1.8.3"]]
-                                  :plugins      [[lein-midje "3.2"]]}
+                                  :plugins      [[lein-midje "3.2.1"]]}
                         :uberjar {:aot :all}}
   :deploy-repositories [
                          ["snapshots" {:url      "https://clojars.org/repo"
@@ -34,7 +34,5 @@
                        ]
   :codox               {
                          :source-uri "https://github.com/pmonks/multigrep/blob/master/{filepath}#L{line}"
-;                         :source-uri "https://github.com/pmonks/multigrep/blob/{version}/{filepath}#L{line}"
-;                         :metadata   {:doc/format :markdown}
                        }
   )
